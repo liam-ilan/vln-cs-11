a51ad7295b5fff923f02cf184be509e30b91d552
Liam Ilan <snowboardsheep@gmail.com>
2020-05-17 16:34:51 -0700

    Completed game loop assignment.

--- Rock Paper Scissors/src/com/company/Main.java
+++ Rock Paper Scissors/src/com/company/Main.java
@@ -12,51 +12,75 @@ import java.util.Scanner;
 */
 
 public class Main {
-
     public static void main(String[] args) {
 
         // init
         Scanner input = new Scanner(System.in);
         String[] choices = {"rock", "paper", "scissors"};
+        int userScore = 0;
+        int botScore = 0;
 
-        // user input
-        System.out.println("Choose rock paper or scissors.\t'r' for rock, 'p' for paper and 's' for scissors.");
-        String user = input.nextLine(); // nextLine catches spaces
+        // opening message
+        System.out.println(
+                "Choose rock paper or scissors.\t" +
+                "'r' for rock, 'p' for paper and 's' for scissors. " +
+                "To exit choose 'x'"
+        );
 
-        // validate
-        if (!(user.equals("r") || user.equals("p") || user.equals("s"))) {
-            System.out.println("Invalid selection please play again.");
-            return;
+        // game loop
+        while (true) {
+
+            // score board
+            System.out.println("*".repeat(46));
+            System.out.println("Wins: " + userScore + "\tLosses: " + botScore);
+
+            // user input
+            System.out.println("Player's Choice:");
+            String user = input.nextLine(); // nextLine catches spaces
+
+            // exit
+            if (user.equals("x")) {
+                System.out.println("Thank you for playing!");
+                break;
+            }
+
+            // validate
+            if (!(user.equals("r") || user.equals("p") || user.equals("s"))) {
+                System.out.println("Invalid selection please play again.");
+                return;
+            }
+
+            // map user input to game choices
+            // note: code easier to read when strings are used, instead of array reference
+            if (user.equals("r")) {
+                user = "rock";
+            } else if (user.equals("p")) {
+                user = "paper";
+            } else {
+                user = "scissors";
+            }
+
+            // bot input
+            String bot = choices[(int) (Math.random() * 3)];
+
+            // play
+            // note: code easier to read when strings are used, instead of array reference
+            Boolean winningCondition =
+                    user.equals("paper") && bot.equals("rock") ||
+                            user.equals("scissors") && bot.equals("paper") ||
+                            user.equals("rock") && bot.equals("scissors");
+
+            if (user.equals(bot)) {
+                System.out.println("Draw!");
+            } else if (winningCondition) {
+                userScore++;
+                System.out.println("You Win!");
+            } else {
+                botScore++;
+                System.out.println("You Lose!");
+            }
+
+            System.out.println("Computer choice: " + bot + "\tPlayer choice: " + user);
         }
-
-        // map user input to game choices
-        // note: code easier to read when strings are used, instead of array reference
-        if (user.equals("r")) {
-            user = "rock";
-        } else if (user.equals("p")) {
-            user = "paper";
-        } else {
-            user = "scissors";
-        }
-
-        // bot input
-        String bot = choices[(int)(Math.random() * 3)];
-
-        // play
-        // note: code easier to read when strings are used, instead of array reference
-        Boolean winningCondition =
-                user.equals("paper") && bot.equals("rock") ||
-                user.equals("scissors") && bot.equals("paper") ||
-                user.equals("rock") && bot.equals("scissors");
-
-        if (user.equals(bot)) {
-            System.out.println("Draw!");
-        } else if (winningCondition) {
-            System.out.println("You Win!");
-        } else {
-            System.out.println("You Lose!");
-        }
-
-        System.out.println("Computer choice: " + bot + "\tPlayer choice: " + user);
     }
 }
