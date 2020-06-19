da808f243de9eaa6f05682d52673b523c5c1d291
Liam Ilan <snowboardsheep@gmail.com>
2020-06-19 12:09:59 -0700

    Got gui-sample to build.

--- gui-sample/src/sample/Controller.java
+++ gui-sample/src/sample/Controller.java
@@ -1,4 +1,23 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class Controller {
+
+    public TextField txtFieldName;
+    public Button btnCancel;
+    public Button btnSubmit;
+    public Label lblOutput;
+
+    public void cancelName(ActionEvent actionEvent) {
+        txtFieldName.setText("");
+        lblOutput.setText("???");
+    }
+
+    public void submitName(ActionEvent actionEvent) {
+        lblOutput.setText(txtFieldName.getText());
+    }
 }
